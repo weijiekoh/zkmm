4b95b2a5d34f2d9900cd5a134c7d7b01367290a0
Koh Wei Jie <weijie.koh@consensys.net>
2018-11-15 18:19:03 +0800

    configured urls

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+
+def json_response(obj):
+    """
+    Returns @obj in JSON format, wrapped in a HttpResponse
+    """
+    return HttpResponse(json.dumps(obj, separators=(',', ':')),
+            content_type="application/json")
+def index(request):
+    return json_response("Mastermind")
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
 from django.urls import path, re_path
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name='index.html'))
+    url(r'^api/', include("app.urls")),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
